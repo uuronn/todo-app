4e3b3540b0ad3a10c3aa700615eb5d5204df729d
yukinissie <bigtree2490@outlook.jp>
2021-11-05 00:47:31 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="container">
+    <h1>Hello, Nuxt template!!!</h1>
+    <p>by {{ companyName }}</p>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      companyName: "Hack'z Inc.",
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
