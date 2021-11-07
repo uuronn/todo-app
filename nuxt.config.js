d12e081da5900071ba0af36bf20cf5a9b862573e
yukinissie <bigtree2490@outlook.jp>
2021-11-07 17:05:21 +0900

    Updated nuxt config js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 export default {
+  mode: 'spa',
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    mode: 'spa',
-    target: 'static',
     title: 'world todo',
     meta: [
       { charset: 'utf-8' },
